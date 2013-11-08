2adcc3d13b194d334487298b99bca8df2964db99
Fernando Garza <nndo1991@gmail.com>
2013-11-07 20:07:17 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greenreference::Application.config.secret_token = 'c29dd239aa47a1de0197b1ea3d5de8ffbe588f6b14104ec7fef372f258a9a4f352e5f3dc1f4c743e3c4d8c5cb9c04206d14cfc230af1cf4b3aa525342c009d92'
